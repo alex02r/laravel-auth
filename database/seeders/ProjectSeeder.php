222548dc8089978153886d78cb3c7320e43a3d68
alex02r <alex.giochi02@gmail.com>
2024-02-21 16:33:30 +0100

    [UPGRADE] Seeder project

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str ;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $new_project = new Project;
+            $new_project->name = $faker->words(3, true);
+            $new_project->description = $faker->paragraph(3, true);
+            $new_project->start_date = $faker->date();
+            $new_project->end_date = $faker->date();
+            $new_project->slug = Str::slug($new_project->name, '-');
+
+            $new_project->save();
+        }
     }
 }
