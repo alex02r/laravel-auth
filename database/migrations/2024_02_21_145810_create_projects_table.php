614bdefae4922039cf0a8e04d2cf2b77f58ee891
alex02r <alex.giochi02@gmail.com>
2024-02-21 16:16:29 +0100

    [UPGRADE] Migration di project

--- database/migrations/2024_02_21_145810_create_projects_table.php
+++ database/migrations/2024_02_21_145810_create_projects_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -15,6 +16,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('slug');
             $table->timestamps();
         });
     }
