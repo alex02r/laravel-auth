fb1b6e8d7bb8617371d6f2cc5d76a7d91c8f8b6f
alex02r <alex.giochi02@gmail.com>
2024-02-21 15:47:50 +0100

    [UPGRADE] Rotta per la dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +20,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/* Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+ */
+Route::middleware(['auth','verified'])
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
