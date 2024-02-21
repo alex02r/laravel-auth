49d3997a45bfaf42fe67e2eda53546de5aa50e5a
alex02r <alex.giochi02@gmail.com>
2024-02-21 17:16:13 +0100

    [UPGRADE]  Aggiunta la rotta per lo show

--- resources/views/admin/project/index-project.blade.php
+++ resources/views/admin/project/index-project.blade.php
@@ -11,11 +11,10 @@
                         <img class="card-img-top p-3" src="https://cdn-icons-png.flaticon.com/512/3767/3767084.png" alt="folder">
                         <div class="card-body">
                           <h5 class="card-title">{{ $project->name}}</h5>
-                          <p class="card-text">{{ $project->desription }}</p>
-                          <span class="text-secondary">Data Creazione: {{ $project->start_date }}</span>
-                          <span class="text-secondary">Data Fine: {{ $project->end_date }}</span>
-                          <div class="">
-                            <a href="{{ route('admin.project.show')}}" class="btn btn-warning">Visualizza</a>
+                          <div class="text-secondary">Data Creazione: {{ $project->start_date }}</div>
+                          <div class="text-secondary">Data Fine: {{ $project->end_date }}</div>
+                          <div class="mt-2">
+                            <a href="{{ route('admin.project.show', ['project' => $project]) }}" class="btn btn-warning">Visualizza</a>
                           </div>
                         </div>
                     </div>
