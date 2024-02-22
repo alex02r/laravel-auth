196944233bcde3270de1b48d1fafce6fa036618d
alex02r <alex.giochi02@gmail.com>
2024-02-22 16:01:07 +0100

    [UPGRADE] Aggiunta la view per la creazione di un nuovo project

---
+++ resources/views/admin/projects/create-project.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-12">
+                <h1>Creazione di un progetto</h1>
+            </div>
+        </div>
+    </div>
+@endsection
