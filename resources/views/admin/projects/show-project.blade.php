ac4452591959487d1e6262e1bbf1c5105eed9cac
alex02r <alex.giochi02@gmail.com>
2024-02-22 15:16:58 +0100

    [UPGRADE] Aggiunta dei pulsanti edit ed elimina

--- resources/views/admin/projects/show-project.blade.php
+++ resources/views/admin/projects/show-project.blade.php
@@ -4,6 +4,12 @@
             <div class="row justify-content-center">
                 <div class="col-6">
                    <div class="card shadow">
+                    <div class="card-header">
+                        <div class="d-flex w-100 justify-content-end gap-2">
+                            <div class="btn btn-sm btn-warning"><a href="{{ route('admin.project.edit', ['project'=>$project]) }}" class="link-dark link-underline-opacity-0">Edit</a></div>
+                            <div class="btn btn-sm btn-danger"><a href="" class="link-light link-underline-opacity-0">Delete</a></div>
+                        </div>
+                    </div>
                         <img class="card-img-start-fluid p-3 w-25" src="https://cdn-icons-png.flaticon.com/512/3767/3767084.png" alt="folder">
                         <div class="card-body">
                           <h5 class="card-title">{{ $project->name}}</h5>
